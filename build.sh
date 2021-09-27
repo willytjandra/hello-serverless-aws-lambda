4ea82525e0fe47ae07a4686a00892c368577b7bb
Willy Tjandra <willy.tjandra@gmail.com>
2021-09-27 20:00:29 +1000

    Fix serverless deployment

--- build.sh
+++ build.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
 
-#install zip on debian OS, since microsoft/dotnet container doesn't have zip by default
-if [ -f /etc/debian_version ]
-then
-  apt -qq update
-  apt -qq -y install zip
-fi
+set -euo pipefail
 
+exec 3>&1
+function say() {
+  printf "%b\n" "[build] $1" >&3
+}
+
+say "Restoring nuget packages"
 dotnet restore
-dotnet tool install -g Amazon.Lambda.Tools --framework netcoreapp3.1
-dotnet lambda package --configuration Release --framework netcoreapp3.1 --output-package bin/Release/netcoreapp3.1/hello.zip
+
+say "Build solution"
+dotnet build -c Release --no-restore
+
+say "Running tests"
+dotnet test -c Release --no-build
+
+say "Packaging lambda"
+dotnet lambda package \
+  --configuration Release \
+  --framework netcoreapp3.1 \
+  --project-location . \
+  --output-package bin/Release/netcoreapp3.1/hello.zip
+
+
